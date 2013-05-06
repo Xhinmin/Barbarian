5a9f949ba85ace9a6ae7cac7a233de12e23dd716
YSL <ganlinoldteather@yahoo.com.tw>
2013-05-06 14:35:19 +0800

    TRY

--- Unity Version/Assets/05_Script/Enemy/EnemyMaker.cs
+++ Unity Version/Assets/05_Script/Enemy/EnemyMaker.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 using System.Collections;
 
 public class EnemyMaker : MonoBehaviour {
-
+	
+	public Transform newEnemy;
+	public bool createEnemy;
+	public Vector2 randonPosition;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,16 @@ public class EnemyMaker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		
+		
+		if(createEnemy)
+		{
+			float newPositionX = Random.Range(randonPosition.x,randonPosition.y);
+			createEnemy = false;
+			Instantiate(newEnemy,new Vector3(newPositionX,0,1),newEnemy.rotation);
+		}
+		
+		
 	}
 }
